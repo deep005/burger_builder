27938655e6676828b3a8cfa830a2c8e40643eae5
Deep Shanker <deep@deep.local>
2018-11-05 07:04:20 +0530

    rendering the burger content dynamically

--- src/containers/burger-builder/burgerBuilder.js
+++ src/containers/burger-builder/burgerBuilder.js
@@ -3,10 +3,21 @@ import Aux from '../../hoc/aux';
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+    /*constructor(props){
+        super(props);
+    }*/
+    state={
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat:2
+        }
+    }
     render(){
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div> Build Controls</div>
             </Aux>
         )
